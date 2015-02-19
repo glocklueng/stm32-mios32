fd44643a6b851fcdf2233262aadb9ccdc3caf80f
syso <syso@micro.no-route.org>
2015-02-19 16:10:25 +0100

    Add setenv script.

---
+++ setenv.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export MIOS32_PATH=/home/syso/mios32/trunk
+export MIOS32_GCC_PREFIX=arm-none-eabi
+export MIOS32_FAMILY=LPC17xx
+export MIOS32_PROCESSOR=LPC1769
+export MIOS32_BOARD=MBHP_CORE_LPC17
+export MIOS32_LCD=universal
